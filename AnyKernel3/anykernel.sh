4faf6437dbd0cf0eaec6e2f20c051fb7a5de961a
Oleg Sazonov <whitylmn@gmail.com>
2019-07-23 15:49:57 +0300

    Добавил нового мейнтейнера :-)

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=CarbonKernel by x0r3d @ 4PDA
+kernel.string=CarbonKernel by x0r3d and nergZd723 @ 4PDA
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
